3b0ef039eec204e685d5d9bd778b2ed170b73c5a
Ruslan <ruslan.13@mail.ru>
2023-08-03 16:07:12 +0300

    Добавили 1 ДЗ

--- seminar4/homework4/Program.cs
+++ seminar4/homework4/Program.cs
@@ -3,16 +3,30 @@
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
 
-Console.Write("Введите первое число: ");
-int numOne = Convert.ToInt32(Console.ReadLine);
+// int PowNum(int numOne, int numTwo)
+// {
+//     int result = 1;
+//     int i = 0;
+//     while(i<numTwo)
+//     {
+//         result = result * numOne;
+//         i++;
+//     }
+//     return result;
+// }
+// Console.Write("Введите первое число: ");
+// int numOne = Convert.ToInt32(Console.ReadLine());
 
-System.Console.WriteLine("Введите второе число: ");
-int numTwo = Convert.ToInt32(Console.ReadLine);
-int result = 0;
-int i = 1;
-while(i<numTwo)
-{
-    result = result * numOne;
-    i++;
-}
-System.Console.WriteLine(result);
+// System.Console.Write("Введите второе число: ");
+// int numTwo = Convert.ToInt32(Console.ReadLine());
+
+// int result = 1;
+// int i = 0;
+// while(i<numTwo)
+// {
+//     result = result * numOne;
+//     i++;
+// }
+// System.Console.WriteLine(result);
+
+// System.Console.WriteLine(PowNum(numOne,numTwo));
