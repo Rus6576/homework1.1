5cffacd1c051ffb8ff2a256e66f53fc1af062f46
Ruslan <ruslan.13@mail.ru>
2023-08-09 15:14:25 +0300

    Добавили 1 задачу

---
+++ seminar5/homework5/Program.cs
@@ -0,0 +1,21 @@
+﻿﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая
+// покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+
+// System.Console.Write("Введите количество элементов в массиве: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int[] array = new int[num];
+// int count = 0;
+// for(int i = 0; i < array.Length; i++)
+// {
+//     array[i] = new Random().Next(100, 1000);
+//     System.Console.Write(array[i] + " ");
+//     if(array[i] % 2 == 0)
+//     {
+//         count++;
+//     }
+// }
+// System.Console.WriteLine("");
+// System.Console.Write(count);
+
