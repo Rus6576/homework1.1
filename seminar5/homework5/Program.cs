92b963de15e53a880022ddbcb829e96a996dcc83
Ruslan <ruslan.13@mail.ru>
2023-08-09 15:55:49 +0300

    Добавили 2 задачу

--- seminar5/homework5/Program.cs
+++ seminar5/homework5/Program.cs
@@ -19,3 +19,24 @@
 // System.Console.WriteLine("");
 // System.Console.Write(count);
 
+
+// Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+System.Console.Write("Введите количество элементов в массиве: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[num];
+int count = 0;
+for(int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(0, 100);
+    System.Console.Write(array[i] + " ");
+    if(i%2 != 0)
+    {
+        count += array[i];
+    }
+}
+System.Console.WriteLine("");
+System.Console.WriteLine(count);
