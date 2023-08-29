2a64bbd74bfe91fb770835d568494584a09f1b4c
Ruslan <ruslan.13@mail.ru>
2023-08-29 11:46:58 +0300

    Добавили 55 задачу

--- seminar8/Program.cs
+++ seminar8/Program.cs
@@ -1,36 +1,88 @@
 ﻿// Задача 53: Задайте двумерный массив. Напишите программу, которая поменяет местами первую и последнюю строку массива.
 
 
+// Console.WriteLine("Первую размерность массива");
+// int rows = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Вторую размерность массива");
+// int coloms = Convert.ToInt32(Console.ReadLine());
+
+// int[,] matrix= new int[rows,coloms];
+
+// for (int i = 0; i < matrix.GetLength(0); i++)
+// {
+//     for (int j = 0; j <  matrix.GetLength(1); j++)
+//     {
+//         matrix[i,j]= new Random().Next(0,10);
+//         Console.Write(matrix[i,j]+ " ");
+//     }
+//     Console.WriteLine();
+// }
+// for(int j =0 ; j < matrix.GetLength(1); j++)
+// {
+//     int changer=matrix.GetLength(0)-1;
+//     int temp= matrix[0,j];
+//     matrix[0,j]=matrix[changer,j];
+//     matrix[changer,j]= temp;
+// }
+// Console.WriteLine();
+// for (int i = 0; i < matrix.GetLength(0); i++)
+// {
+//     for (int j = 0; j <  matrix.GetLength(1); j++)
+//     {
+//         Console.Write(matrix[i,j]+ " ");
+//     }
+//     Console.WriteLine();
+// }
+
+
+// Задача 55: Задайте двумерный массив. Напишите программу,
+//  которая заменяет строки на столбцы. В случае, если это
+//   невозможно, программа должна вывести сообщение для пользователя.
+
 Console.WriteLine("Первую размерность массива");
 int rows = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Вторую размерность массива");
-int coloms = Convert.ToInt32(Console.ReadLine());
+int[,] matrix= new int[rows,rows];
 
-int[,] matrix= new int[rows,coloms];
+int[,] matrix2= new int[rows,rows];
 
-for (int i = 0; i < matrix.GetLength(0); i++)
+void CreateArray()
 {
-    for (int j = 0; j <  matrix.GetLength(1); j++)
+    for (int i = 0; i < matrix.GetLength(0); i++)
     {
-        matrix[i,j]= new Random().Next(0,10);
-        Console.Write(matrix[i,j]+ " ");
+        for (int j = 0; j <  matrix.GetLength(1); j++)
+        {
+            matrix[i,j]= new Random().Next(0,10);
+        }
     }
-    Console.WriteLine();
 }
-for(int j =0 ; j < matrix.GetLength(1); j++)
+
+void PrintArray(int[,] array)
 {
-    int changer=matrix.GetLength(0)-1;
-    int temp= matrix[0,j];
-    matrix[0,j]=matrix[changer,j];
-    matrix[changer,j]= temp;
+    for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j <  array.GetLength(1); j++)
+            {
+                Console.Write(array[i,j]+ " ");
+            }
+            Console.WriteLine();
+        }
 }
+
+void ChangeArray()
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j <  matrix.GetLength(1); j++)
+        {
+            matrix2[j,i]= matrix[i,j];
+        }
+    }
+}
+
+CreateArray();
+PrintArray(matrix);
 Console.WriteLine();
-for (int i = 0; i < matrix.GetLength(0); i++)
-{
-    for (int j = 0; j <  matrix.GetLength(1); j++)
-    {
-        Console.Write(matrix[i,j]+ " ");
-    }
-    Console.WriteLine();
-}
+ChangeArray();
+PrintArray(matrix2);
