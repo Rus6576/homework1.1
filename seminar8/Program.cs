92ef93fbec10d15af1eee1ccdfa37c27f37f13dd
Ruslan <ruslan.13@mail.ru>
2023-08-29 11:44:43 +0300

    Добавили 53 задачу

---
+++ seminar8/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 53: Задайте двумерный массив. Напишите программу, которая поменяет местами первую и последнюю строку массива.
+
+
+Console.WriteLine("Первую размерность массива");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Вторую размерность массива");
+int coloms = Convert.ToInt32(Console.ReadLine());
+
+int[,] matrix= new int[rows,coloms];
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j <  matrix.GetLength(1); j++)
+    {
+        matrix[i,j]= new Random().Next(0,10);
+        Console.Write(matrix[i,j]+ " ");
+    }
+    Console.WriteLine();
+}
+for(int j =0 ; j < matrix.GetLength(1); j++)
+{
+    int changer=matrix.GetLength(0)-1;
+    int temp= matrix[0,j];
+    matrix[0,j]=matrix[changer,j];
+    matrix[changer,j]= temp;
+}
+Console.WriteLine();
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j <  matrix.GetLength(1); j++)
+    {
+        Console.Write(matrix[i,j]+ " ");
+    }
+    Console.WriteLine();
+}
