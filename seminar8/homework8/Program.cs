e7cf5c7b446bdb97d118a634693db8f475437cd8
Ruslan <ruslan.13@mail.ru>
2023-09-03 17:36:23 +0300

    Добавили задачу 56

--- seminar8/homework8/Program.cs
+++ seminar8/homework8/Program.cs
@@ -74,3 +74,78 @@
 // OrderingArray(matrix);
 // PrintArray(matrix);
 
+
+
+    // Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей
+    // суммой элементов.
+
+    // Например, задан массив:
+    // 1 4 7 2
+    // 5 9 2 3
+    // 8 4 2 4
+    // 5 2 6 7
+    // Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+
+// System.Console.Write("Введите первую размерность массива: ");
+// int rows = Convert.ToInt32(System.Console.ReadLine());
+// System.Console.Write("Введите первую размерность массива: ");
+// int colums = Convert.ToInt32(System.Console.ReadLine());
+// int[,] matrix = new int[rows, colums];
+// void InputArray(int[,] array)
+// {
+//     for(int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for(int j = 0; j < array.GetLength(1); j++)
+//         {
+//             array[i, j] = new Random().Next(10, 100);
+//         }
+//     }
+// }
+// void PrintArray(int[,] array)
+// {
+//     for(int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for(int j = 0; j < array.GetLength(1); j++)
+//         {
+//             if(j == 0)
+//             {
+//                 System.Console.Write("[");
+//             }
+//             System.Console.Write(array[i,j]);
+//             if(j == array.GetLength(1) - 1)
+//             {
+//                 System.Console.Write("]");
+//             }
+//             else
+//             {
+//                 System.Console.Write("; ");
+//             }
+//         }
+//         System.Console.WriteLine();
+//     }
+// }
+// void SumElementArray(int[,] array)
+// {
+//     int min = int.MaxValue;
+//     int minLine = 0;
+//     for(int i = 0; i < array.GetLength(0); i++)
+//     {
+//         int sum = 0;
+//         for(int j = 0; j < array.GetLength(1); j++)
+//         {
+//             sum += array[i,j];
+//         }
+//         System.Console.WriteLine($"Сумма элементов {i+1} строки равняется - {sum}.");
+//         if(sum < min)
+//         {
+//             min = sum;
+//             minLine = i+1;
+//         }
+//     }
+//     System.Console.WriteLine($"{minLine} строка имеет наименьшую сумму элементов.");
+// }
+// InputArray(matrix);
+// PrintArray(matrix);
+// System.Console.WriteLine();
+// SumElementArray(matrix);
