6f8b57d1103bc9d6a96c724f43e1e6000d4fd534
Ruslan <ruslan.13@mail.ru>
2023-09-13 20:06:26 +0300

    Добавили задачу 66

--- seminar9/homework9/Program.cs
+++ seminar9/homework9/Program.cs
@@ -18,3 +18,42 @@
 //     }
 //         return (end + " " + PrintNumbers(end - 1,start));
 // }
+
+
+// Задача 66: Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.WriteLine("Введите начальное число: ");
+int num1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите конечное число: ");
+int num2 = int.Parse(Console.ReadLine());
+
+string PrintNumbers(int num1 , int num2)
+    {
+    if(num1 == num2)
+    {
+        return num1.ToString();
+    }
+        return num1 + " " + PrintNumbers(num1+1, num2);
+    }
+
+if (num1 > num2)
+{
+    Console.WriteLine("Первое число должно быть меньше второго!");
+}
+else
+{
+    Console.WriteLine("Массив состоит из чисел" +PrintNumbers(num1,num2));
+
+    int sum = 0;
+    while (num1<=num2)
+    {
+        sum+=num1;
+        num1++;
+    }
+    Console.WriteLine($"Сумма чисел массиваравна {sum}");
+}
+
