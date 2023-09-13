334f4fbe5759619d9955999b0a1c6403ddca8033
Ruslan <ruslan.13@mail.ru>
2023-09-13 19:41:39 +0300

    Добавили задачу 64

--- seminar9/homework9/Program.cs
+++ seminar9/homework9/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 64: Задайте значение N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+// Console.Write("Введите число: ");
+// int num = int.Parse(Console.ReadLine());
+
+// int startNum = 1;
+
+// Console.WriteLine(PrintNumbers(num ,startNum));
+
+// string PrintNumbers(int end, int start)
+// {
+//     if(end == start)
+//     {
+//         return start.ToString();
+//     }
+//         return (end + " " + PrintNumbers(end - 1,start));
+// }
